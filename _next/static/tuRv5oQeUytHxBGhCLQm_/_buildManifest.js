3fe78051352cbbaa5f6af1ae1b6a946b91168a98
Nabeel Nizam <abee237@gmail.com>
2026-04-26 15:23:19 +0700

    Updates

--- _next/static/tuRv5oQeUytHxBGhCLQm_/_buildManifest.js
+++ _next/static/tuRv5oQeUytHxBGhCLQm_/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/PortofolioNabeel//_next/:path+",
+        "destination": "/PortofolioNabeel/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
